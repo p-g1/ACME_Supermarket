6ca3195fbacecb79d017ea3f46b863cd0a22a293
Pete <petergarvin1@gmail.com>
2019-08-01 21:56:35 +0100

    initial commit

---
+++ ACME_Supermarket.js
@@ -0,0 +1,67 @@
+const items = [
+        {
+            "code": "FR1",
+            "name": "Fruit tea",
+            "price": 3.11,
+        },
+        {
+            "code": "SR1",
+            "name": "Strawberries",
+            "price": 5.00,
+        },
+        {
+            "code": "CF1",
+            "name": "Coffee",
+            "price": 11.23,
+        }
+    ];
+
+const twoForOneItemCodes = ["FR1"];
+const multibuyOffers = [{code:"SR1", quantity: 3, discountedPrice: 4.50}];
+        
+const twoForOneChecker = item => twoForOneItemCodes.some(code => code === item);
+
+const multibuyChecker = (item, quantity) => {
+    const discount = multibuyOffers.filter(offer => offer.code === item && offer.quantity <= quantity);
+
+    return discount.length > 0 ? discount[0].discountedPrice : null;
+}
+
+const pricingRules = {
+    "items": items,
+    "twoForOneItemCodes": twoForOneItemCodes,
+    "multibuyOffers": multibuyOffers,
+    "twoForOneChecker" : twoForOneChecker,
+    "multibuyChecker": multibuyChecker
+};
+
+class Basket {
+    constructor(pricingRules) {
+    
+    this.shoppingBasket = [];
+
+    this.add = item => {
+        this.shoppingBasket.push(item);
+    }
+
+    this.total = () => {
+        const uniqueBasketItems = [...new Set(this.shoppingBasket)];
+        let total = 0;
+
+        uniqueBasketItems.forEach(item => {
+            const itemCount = this.shoppingBasket.filter(basketItem => basketItem === item).length;
+            const twoForOneResult = pricingRules.twoForOneChecker(item);
+            const multibuyResult = pricingRules.multibuyChecker(item, itemCount);
+            const price = pricingRules.items.filter(object => object.code === item)[0].price;
+            
+            let itemTotal = multibuyResult ? itemCount * multibuyResult 
+                          : twoForOneResult ? (Math.floor(itemCount / 2) + (itemCount % 2)) * price
+                          : itemCount * price;
+            
+            total += itemTotal;
+        })
+        
+        return "£" + total.toFixed(2);
+        }
+    }
+}
